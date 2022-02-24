77a4a2787a7f0472acc29402c9d98314633f21e8
Jack Pena <penajackc@gmail.com>
2022-02-24 10:17:46 -0700

    commit tree 0b337060086f671b64af06ed6d421373b378737f

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,38 @@
+class CashRegister
 
+    attr_reader :discount
+    attr_accessor :total
+
+    @@last_price = 0
+
+    def initialize(discount = 0)
+        @total = 0;
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        @total += price * quantity
+        quantity.times do |i|
+            @items << title
+        end
+        @@last_price = price * quantity
+    end
+
+    def apply_discount
+        if @discount > 0
+            @total = total - (total * discount / 100)
+            "After the discount, the total comes to $#{@total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def items
+        @items
+    end
+
+    def void_last_transaction
+        @total -= @@last_price
+    end
+end
